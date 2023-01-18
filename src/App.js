3e4984cc430122a318005e041b8203e5a5fa32cb
rdtungul <rosephdarl@gmail.com>
2023-01-18 23:03:02 +0800

    Adding some changes inside the App.js code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const data = {
+    url: "https://rdtungul.github.io/",
+  };
+  const Header = () => {
+    return (
+      <div>
+        <h1>Roseph Darl</h1>
+        <a href={data.url} target="_blank">
+          Link to my portfolio
+        </a>
+      </div>
+    );
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
